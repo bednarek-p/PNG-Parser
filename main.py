fd0dc5e06247c07a9ed060afbafa61248db83412
klawik-j <kklawik345@gmail.com>
2021-04-10 13:24:49 +0200

    anonymization

--- main.py
+++ main.py
@@ -12,15 +12,17 @@ if png_file.read(len(Decoder.SIGNATURE)) != Decoder.SIGNATURE:
     raise Exception('Its not PNG file')
 else:
     png = Decoder(png_file, png_cv2_image)
-    # png.print_chunks_type()
+    #png.print_chunks_type()
     # png.IEND_print_chunk_data()
     # png.print_chunks_type()
     # png.IHDR_print_chunk_data()
     # png.IHDR_print_chunk_formated_data()
     # png.IDAT_plot_image()
-    # png.IEND_print_chunk_data()
+    # png.IEND_print_chunk_data(
     # png.SRGB_print_chunk_data()
     # png.SRGB_print_chunk_formated_data()
     # png.PLTE_print_chunk_data()
     # png.PLTE_print_chunk_formated_data()
     png.Spectrum_show_images()
+    #png.anonymization()
+
