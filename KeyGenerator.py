9ea1fd05be8dddd5e19b1e965074e53909c53b83
klawik-j <kklawik345@gmail.com>
2021-05-07 22:45:59 +0200

    change the way to confirm if the numer is prime in is_prime(), used Miller-Rabin to improve time complexity

--- KeyGenerator.py
+++ KeyGenerator.py
@@ -1,6 +1,9 @@
 import random
 
 class KeyGenerator:
+    """
+    n = pq
+    """
     def __init__(self, key_size):
         self.key_size = key_size
         self.n, self.e, self.d = 0,0,0
@@ -9,20 +12,45 @@ class KeyGenerator:
     @classmethod
     def is_prime(self, number):
         """
-        Fuction check if the number is prime number
+        source: https://rosettacode.org/wiki/Miller%E2%80%93Rabin_primality_test#Python:_Probably_correct_answers
+        
+        Miller-Rabin primality test.
+    
+        A return value of False means n is certainly not prime. A return value of
+        True means n is very likely a prime.
         """
-        if number == 1: return False
-        if number == 2: return True
-        if number == 3: return True
-        if number % 2 == 0 or number % 3 == 0: return False
-        i = 5
-        while i < number/2:
-            if number % i == 0 or number % (i+2) == 0: return False
-            i = i+6
-        return True
+        #Miller-Rabin test for prime
+        if number==0 or number==1 or number==4 or number==6 or number==8 or number==9:
+            return False
+    
+        if number==2 or number==3 or number==5 or number==7:
+            return True
+        s = 0
+        d = number-1
+        while d%2==0:
+            d>>=1
+            s+=1
+        assert(2**s * d == number-1)
+    
+        def trial_composite(a):
+            if pow(a, d, number) == 1:
+                return False
+            for i in range(s):
+                if pow(a, 2**i * d, number) == number-1:
+                    return False
+            return True  
+    
+        for i in range(8):#number of trials 
+            a = random.randrange(2, number)
+            if trial_composite(a):
+                return False
+    
+        return True 
 
     @classmethod
     def prime_generator(self, prime_binary_size):
         while True:
             number = random.randrange(2**(prime_binary_size-1), 2**prime_binary_size-1)
+            print(number)
             if KeyGenerator.is_prime(number): return number
+
