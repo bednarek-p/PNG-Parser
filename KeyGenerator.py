3b283589af0b21a619d229707b312215a3661dfa
klawik-j <kklawik345@gmail.com>
2021-05-07 22:10:36 +0200

    add prime number generator

--- KeyGenerator.py
+++ KeyGenerator.py
@@ -1,8 +1,10 @@
+import random
+
 class KeyGenerator:
     def __init__(self, key_size):
         self.key_size = key_size
         self.n, self.e, self.d = 0,0,0
-        self.prime_size = key_size/2 #key is made by two prime numbers
+        self.prime_binary_size = key_binary_size/2 #key is made by two prime numbers
 
     @classmethod
     def is_prime(self, number):
@@ -18,3 +20,9 @@ class KeyGenerator:
             if number % i == 0 or number % (i+2) == 0: return False
             i = i+6
         return True
+
+    @classmethod
+    def prime_generator(self, prime_binary_size):
+        while True:
+            number = random.randrange(2**(prime_binary_size-1), 2**prime_binary_size-1)
+            if KeyGenerator.is_prime(number): return number
